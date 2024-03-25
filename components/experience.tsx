22dbc84275cb46ce582c0ddb89e521e76c633307
hvtinh <hoangvutinh1@gmail.com>
2024-03-25 20:59:51 +0700

    Fix mobile responsive

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor=""  className="!w-[1000px]">
+      <VerticalTimeline lineColor=""  className="!sm:w-[1000px]">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
